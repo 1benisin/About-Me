d9e22380cda94084ee6024266adbdafaddbca03c
Benjamin Clark <benisin@gmail.com>
2019-06-04 14:39:03 -0700

    Have gotten JS propting user with 4 question and logging them

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 
 'use strict';   
 
+
+var q1Truth = 'N';
+var q2Truth = 'Y';
+var q3Truth = 'Y';
+var q4Truth = 'Y';
+var q5Truth = 'Y';
+
+var q1Answer = prompt('(Y/N) Is my full name Benjamin James Clark?').toUpperCase();
+console.log(q1Answer);
+var q2Answer = prompt('(Y/N) Do I have more than 5 siblings?').toUpperCase();
+console.log(q2Answer);
+var q3Answer = prompt('(Y/N) Do I live with parents?').toUpperCase();
+console.log(q3Answer);
+var q4Answer = prompt('(Y/N) Have I ever had a parachute malfunction while jumping from a plane?').toUpperCase();
+console.log(q4Answer);
+var q5Answer = prompt('(Y/N) Will you be my neighbor?').toUpperCase();
+console.log(q5Answer);
+
+
+var q1Response = 'No, my son\'s middle name is James. Mine is Jacob.';
